06c4897968995eccd08c6813896d72e811b97993
javier salcedo <javiersalcedo75@gmail.com>
2016-02-20 12:06:01 -0300

    render p5js working

--- public/js/sketch.js
+++ public/js/sketch.js
@@ -2,9 +2,33 @@
   'use strict';
 
 
-  angular.module('hacksummit').controller('SketchCtrl', ['$scope', function($scope) { 
-  	 
+  angular.module('hacksummit').controller('SketchCtrl', ['$scope','$http','$mdDialog','$state','$rootScope','$location',function($scope,$http,$mdDialog,$state,$rootScope,$location) {
+ //Firebase reference
+    $scope.syncRef;
 
+    //Object to use across the application
+    $scope.data = {};
+   
+    /**
+     * Init everything needed here!!!
+     * @return     {[type]}                 [description]
+     */
+    function initialise() {
+       $scope.syncRef = new Firebase("https://hash2016.firebaseio.com/"+$state.params.identifier);      
+      //Read and sync with what Firebase has
+      $scope.syncRef.on('value', function(dataSnapshot) { 
+		       $scope.data = dataSnapshot.val();
+		       var tagcode = document.querySelector('#code');
+		       tagcode.innerHTML = $scope.data.code;
+		       renderCode('displaycode');
+      });
+    }
+
+    if($state.params.identifier){
+    	initialise();   	
+    }else{
+    	$location.path('/dashboard');
+    }
 
   }]);
 
